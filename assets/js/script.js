d01fc2f40463479f0680f46cf890643a4822664f
kstensing <stensingk@gmail.com>
2022-01-15 23:16:43 -0600

    added time reduction and score tracker

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@ var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 var score = 0;
 var i = 0;
+var timeLeft = 75;
 
+// button to begin the timer
 var buttonEl = document.querySelector("#proceed-button");
 buttonEl.addEventListener("click", function() {
     countdown()
@@ -10,10 +12,9 @@ buttonEl.addEventListener("click", function() {
     });
 
 function countdown() {
-    var timeLeft = 75;
 
-    var timeInterval = setInterval(function() {
-        
+    var timeInterval = setInterval (function() {
+        // adding time left to the timer element
         timerEl.textContent = "Time: " + timeLeft + " seconds"
         timeLeft--;
 
@@ -28,12 +29,6 @@ function countdown() {
 // var testQue = Object.keys(answer1);
 // console.log(testQue)
 
-
-// //iterate over keys
-// for (let i = 0; i< testVar.length; i++) {
-//     testVar2 = testVar[i] //iterate over key
-//     testvar3 = scores.[testVar2]; // passing key into
-// }
 
  
 var questionBank = [
@@ -88,7 +83,7 @@ var questionBank = [
 
 
 function nextQuestion() {
-    
+    // if i is less than the questionBank, continue on to the next question
     if (i < questionBank.length) {
         i++;
         document.getElementById("question").innerText = questionBank[i].questionText
@@ -105,50 +100,68 @@ function nextQuestion() {
 
 
 function correct (){
-    //when button[i] == correctAnswer
-    alert("ya")
 
     // element to appear that says "correct!"
+    document.getElementById("message").innerText = "Correct!"
 
     // add score++
+    score++
+    console.log(score)
 
     // proceed with nextQuestion
+    nextQuestion()
+    
+
 };
 
 function incorrect (){
-    alert("nah")
+    // element to appear that says "wrong!"
+    document.getElementById("message").innerText = "Wrong!"
+
+    // subtract time 
+    timeLeft = timeLeft - 10;
+
+    // proceed with nextQuestion
+    nextQuestion()
+    
 };
 
 
 function startGame() {
+    // changing the welcome statement to question 1
     document.getElementById("question").innerText = questionBank[i].questionText;
 
+    // creating the buttons showing answer options
     document.getElementById("quiz-contents").appendChild
     var button0 = document.createElement("button");
     var button1 = document.createElement("button");
     var button2 = document.createElement("button");
     var button3 = document.createElement("button");
+    // setting the text content of the buttons to the appropriate answer option from the array
     button0.textContent = questionBank[i].options[0];
     button1.textContent = questionBank[i].options[1];
     button2.textContent = questionBank[i].options[2];
     button3.textContent = questionBank[i].options[3];
+    // adding the button class names for styling 
     button0.className = "btn";
     button1.className = "btn";
     button2.className = "btn";
     button3.className = "btn";
+    // adding the button id's for selecting use later
     button0.id = "b1";
     button1.id = "b2";
     button2.id = "b3";
     button3.id = "b4";
+    // appending the new button to the quiz-content elements
     document.getElementById("quiz-contents").appendChild(button0);
     document.getElementById("quiz-contents").appendChild(button1);
     document.getElementById("quiz-contents").appendChild(button2);
     document.getElementById("quiz-contents").appendChild(button3);
-
+    // removing the intro P and Begin Quiz button
     document.getElementById("intro").remove()
     document.getElementById("proceed-button").remove()
 
- 
+    // add event listeners to know when the correct answer is hit to proceed with the "correct" function
     switch (questionBank[i].correctAnswer) {
         case 0:
             button0.addEventListener("click", correct);
@@ -164,6 +177,7 @@ function startGame() {
             break;
     } 
 
+    // add event listeners to the buttons to move to the "incorrect" function when the incorrect answer button is selected
     if (questionBank[i].correctAnswer !== 0) {
         button0.addEventListener("click", incorrect)
     } 
